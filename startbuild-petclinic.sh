55915854315bf3e4ea10a69f20e064e91074c8f9
Enrico Welder <enrico.welder@rws.nl>
2021-06-14 17:19:42 +0200

    use tekton-pipelines ns

--- startbuild-petclinic.sh
+++ startbuild-petclinic.sh
@@ -3,5 +3,5 @@
        --workspace name=argo-app-ws,volumeClaimTemplateFile=workspace-template.yaml \
        --workspace name=git-app-config-ssh-creds,secret=github-pipeline-config-petclinic-ssh-key \
        --use-param-defaults \
-       --namespace default \
+       --namespace tekton-pipelines \
        --pod-template pod-template.yaml
